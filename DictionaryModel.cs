5b8e1e70beabae59b6c3d22e3b5ddef8fa8feb8e
Artem Levin <leoartemlev@yandex.ru>
2022-05-10 20:12:09 +0500

    Добавил класс, работающий с XML

---
+++ DictionaryModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartDictionary
+{
+    public struct Word
+    {
+        public string word { get; set; }
+        public string translation { get; set; }
+    }
+
+    public class DictionaryModel
+    {
+        public bool AddNewWord(string word, string translate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveWord(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Word GetRandomWord()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ XmlWorker.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+
+
+namespace SmartDictionary.XML
+{
+    public class XmlWorker
+    {
+        const string fileName = "Dict.xml";
+
+
+        /// <summary>
+        /// Добавляет новую запись в XML файл
+        /// </summary>
+        /// <param name="word">Слово</param>
+        /// <param name="translation">Перевод слова</param>
+        public static void AddRecord(string word, string translation)
+        {
+            XDocument doc;
+
+            if (!File.Exists(fileName))
+            {
+                doc = new XDocument();
+                doc.Add(new XElement("Dict"));
+            }
+            else
+                doc = XDocument.Load(fileName);
+
+            var root = doc.Root;
+
+
+            if (root != null)
+                root.Add(new XElement("Record"), new XAttribute("word", word), new XAttribute("translation", translation));
+
+            doc.Save(fileName);
+
+        }
+
+
+        /// <summary>
+        /// Получение перевода конкретного слова
+        /// </summary>
+        /// <param name="word">Слово</param>
+        /// <returns></returns>
+        /// <exception cref="FileNotFoundException"></exception>
+        /// <exception cref="Exception"></exception>
+        public static string GetTranslation(string word)
+        {
+            if (!File.Exists(fileName))
+                throw new FileNotFoundException(fileName);
+
+            XDocument doc = XDocument.Load(fileName);
+
+            var root = doc.Root;
+
+            foreach (var record in root.Elements("Record"))
+            {
+                if (record.Attribute("word") != null)
+                    if (record.Attribute("word").Value == word)
+                        return record.Attribute("translation").Value;
+            }
+
+            throw new Exception("Word not found");
+        }
+
+
+        /// <summary>
+        /// Получение слова по его переводу
+        /// </summary>
+        /// <param name="translation">Перевод слова</param>
+        /// <returns></returns>
+        /// <exception cref="FileNotFoundException"></exception>
+        /// <exception cref="Exception"></exception>
+        public static string GetWord(string translation)
+        {
+            if (!File.Exists(fileName))
+                throw new FileNotFoundException();
+
+            XDocument doc = XDocument.Load(fileName);
+
+            var root = doc.Root;
+
+            foreach (var record in root.Elements("Record"))
+            {
+                if (record.Attribute("translation") != null)
+                    if (record.Attribute("translation").Value == translation)
+                        return record.Attribute("word").Value;
+            }
+
+            throw new Exception("Translation not found");
+        }
+    }
+}
